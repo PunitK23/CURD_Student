ce50496b6d256211fa61b1397953eb091ad94647
Punit <prkasundra@gmail.com>
2023-05-24 18:47:21 +0530

    Initial Commit

---
+++ src/main/java/com/punitit/CurdStudentApplication.java
@@ -0,0 +1,13 @@
+package com.punitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdStudentApplication.class, args);
+	}
+
+}
